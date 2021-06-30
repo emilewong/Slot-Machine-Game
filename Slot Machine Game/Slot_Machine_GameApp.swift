26c5a318dbd5394dfc42871b3aca1a649ede91b5
Emile Wong <emilewong@outlook.com>
2021-06-30 20:20:53 +0800

    Initial Commit

---
+++ Slot Machine Game/Slot_Machine_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine_GameApp.swift
+//  Slot Machine Game
+//
+//  Created by Emile Wong on 30/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
